5bad9fd302c27789e43cad0a06d0b233c6ad53ac
BrandonKim <Brandoisthy@Berkeley.edu>
2026-04-02 21:35:47 -0700

    Changed pytest in test_codon_checker.py because it used wrong values for codon_diversity, utilizing hte older version (which isn't unique_codons/62).

--- tests/unit/checkers/test_codon_checker.py
+++ tests/unit/checkers/test_codon_checker.py
@@ -18,7 +18,7 @@ def test_high_cai_example(codon_checker):
     codons_above_board, codon_diversity, rare_codon_count, cai_value = codon_checker.run(cds)
     
     assert codons_above_board == True
-    assert codon_diversity == 1.0
+    assert codon_diversity == 0.06451612903225806 ###4/62, because there are 4 unique codons and 62 total codons. Changed to match how codon diversity is calculated in codon checker.py
     assert rare_codon_count == 0
     assert cai_value > 0.5
 
@@ -30,7 +30,7 @@ def test_low_cai_example(codon_checker):
     codons_above_board, codon_diversity, rare_codon_count, cai_value = codon_checker.run(cds)
 
     assert codons_above_board == False
-    assert codon_diversity == 0.5
+    assert codon_diversity == 0.03225806451612903  ###2/62, because there are 2 unique codons and 62 total codons. Changed to match how codon diversity is calculated in codon checker.py
     assert rare_codon_count == 4
     assert cai_value < 0.1
 
@@ -54,6 +54,6 @@ def test_medium_cai_example(codon_checker):
     codons_above_board, codon_diversity, rare_codon_count, cai_value = codon_checker.run(cds)
 
     assert codons_above_board == True
-    assert codon_diversity > 0.7
+    assert codon_diversity > 0.06451612903225806 ###4/62, because there are 4 unique codons and 62 total codons. Changed to match how codon diversity is calculated in codon checker.py
     assert rare_codon_count == 0
     assert cai_value > 0.2
